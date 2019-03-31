eb4cec9d0d5179b4d24845865a3f1c75709e2bfc
Srinarayana Tantry <narayana.tantry7@gmail.com>
2019-04-01 03:29:01 +0530

    Add files via upload

---
+++ jumbledwordsgame.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 06:20:52 2019
+
+@author: naray
+"""
+
+import random 
+def choose():
+    words=['eat','water','computer','love','python','GOD','discipline']
+    pick=random.choice(words)
+
+def jumbleword(words):
+    "".join(random.sample(word,len(word)))
+    return jumble
+
+   
