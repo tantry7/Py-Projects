6b696bceee194d0d0e30e98ee8abd59d474fb76a
Srinarayana Tantry <narayana.tantry7@gmail.com>
2019-04-01 03:58:49 +0530

    Update jumbledwordsgame.py

--- jumbledwordsgame.py
+++ jumbledwordsgame.py
@@ -14,4 +14,43 @@ def jumbleword(words):
     "".join(random.sample(word,len(word)))
     return jumble
 
+def play():
+p1 = input('your name ')
+p2 = input('your name')
+pp1 = 0
+pp2 = 0
+t = 0
+while(1):
+    picked_word:choose()
+    qn= jumble(picked_word)
+    print qn
+        
+if t%2==0: 
+    print(p1,"your turn.")
+    ans = input("what is on your mind")
+    if ans==picked_word:
+        pp1+=
+        print('your score is',pp1)
+    else:
+        print("better luck next time)
+    c = input("press 1 to continue and to quit :")
+    if c==0:
+         break
+              
+else:
+    print(p1,"your turn.")
+    ans = input("what is on your mind")
+    if ans==picked_word:
+        pp1+=
+        print('your score is',pp1)
+    else:
+        print("better luck next time)
+    c = input("press 1 to continue and to quit :")
+    if c==0:
+          break
+           
+              
+        
+    
+
    
