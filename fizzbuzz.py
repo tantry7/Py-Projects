10eb72ada901abd6a107c941f8c8775d00040ef8
Srinarayana Tantry <narayana.tantry7@gmail.com>
2019-04-01 03:26:13 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created one(1,23): Wed Mar  6 01:37:26 2019
+
+@author: naray
+"""
+#n=input('input number 1')
+#m=input('input number 2')
+#x=input('the number which counting should start from')
+#y=input('the number which it has to end at')
+for i in range(x,y):
+    if(i%3==0 and i%5==0):
+            print(str(i)+"=  FIZZ BUZZ")
+            else:
+                if(i%3==0):
+                       print(str(i)+"= FIZZ")
+                   else: 
+                       if(i%5==0):
+                           print(str(i)+"= BUZZ")
+                       else:
+                           print(str(i))
+                      
+        
+                
+            
+        
+             
+    
+        
+             
+    
+                   
+         
+                      
+                   
