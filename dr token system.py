a71d2391a29a1fddebc6b872a319998a4d19adef
Srinarayana Tantry <narayana.tantry7@gmail.com>
2019-04-01 03:27:34 +0530

    Add files via upload
    A token system which can be used by my local doctor

---
+++ dr token system.py
@@ -0,0 +1,10 @@
+print"good morning, welcome to DR achars shop"
+c=1
+n=1
+while(c==1):
+    print"the person with token",n,"can come in"
+    c=input("next person(0/1)?")
+    c=int(c)
+    n=n+1
+    print"pls come on the next daY"
+1    
